f11805f43bd00ad7e8d18f79e570854d4ebe5090
Robbin van der Peijl <robbin@sowiso.com>
2024-07-10 22:48:02 +0200

    Rename framework to context for broader usecases

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 // import dotenv from 'dotenv';
 dotenv.config();
 
-const framework = process.env.FRAMEWORK || "";
+const CONTEXT = process.env.CONTEXT || "";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:4321" + `/${framework}`,
+    baseURL: "http://localhost:4321" + `/${CONTEXT}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -75,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://localhost:4321" + `/${framework}`,
+    url: "http://localhost:4321" + `/${CONTEXT}`,
     reuseExistingServer: !process.env.CI,
   },
 });
